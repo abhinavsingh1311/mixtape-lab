98f3bbe8f65d6e116c1362ab88af3905f742f9e0
Abhinav singh <asingh308@nait.ca>
2025-02-19 23:08:19 -0700

    FIXED TAILWIND ERRORS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,19 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        'neon-blue': '#00f3ff',
+        'neon-cyan': '#00ffff',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
         sans: ['Starjedi', 'ui-sans-serif', 'system-ui'],
@@ -19,5 +21,4 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-export default config;
+}
